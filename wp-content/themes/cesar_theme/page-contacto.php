3d1db6366cb0d2f64ebaea3a9b1591ec28a2cbc0
Mary Martinez <mary.angmart@gmail.com>
2019-08-20 13:08:31 -0600

    Finishing Contact Page

--- wp-content/themes/cesar_theme/page-contacto.php
+++ wp-content/themes/cesar_theme/page-contacto.php
@@ -1,10 +1,20 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-  <div class="contact__container"> 
-    <div class="container pt-5 contact__content">
-      <h1 class="contact__title"> <?php the_title() ?> </h1>
-      <?php the_content() ?>
+  <?php 
+    $id = get_the_ID();
+    $banner = get_field('banner', $id);
+  ?>
+
+  <div class="contact__container">
+    <div class="contact__container-left">
+      <img src="<?= $banner ?>" alt="Cesar Jara Picture">
+    </div>
+    <div class="contact__container-right">
+      <div class="pt-5 px-5 contact__content">
+        <h1 class="contact__title"> <?php the_title() ?> </h1>
+        <?php the_content() ?>
+      </div>
     </div>
   </div>
 <?php endwhile; endif; ?>
